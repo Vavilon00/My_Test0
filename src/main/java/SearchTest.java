0af3f48b7e83c0e431de1431de6b1b59713ffeb3
DD <123>
2024-02-23 10:36:40 +0300

    First commit

---
+++ src/main/java/SearchTest.java
@@ -0,0 +1,18 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTest {
+
+
+    @Test
+    void successfulSearchTest() {
+        Configuration.holdBrowserOpen=true;
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
